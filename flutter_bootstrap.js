2d22d3ff6f4c3bd3bd58126ce109fee59dedd720
Mikhail Sirotyuk <michail.sirotyuk@krainet.by>
2025-01-18 00:07:33 +0300

    les1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2436921433"
+    serviceWorkerVersion: "264916137"
   }
 });
